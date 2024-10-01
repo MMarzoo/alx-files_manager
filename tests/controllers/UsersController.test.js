25ae78a593e6fc7cd0511cf70d176410411ea3cd
MMarzoo <mmarzouk12378@gmail.com>
2024-10-02 00:11:29 +0300

    Task#10: GET /users/me

--- tests/controllers/UsersController.test.js
+++ tests/controllers/UsersController.test.js
@@ -1,23 +1,24 @@
 import request from 'supertest';
-import { expect, use } from 'chai';
+import { expect } from 'chai';
 import { ObjectId } from 'mongodb';
 import dbClient from '../../utils/db';
-import redisClient from '../../utils/redis';
 import app from '../../server';
 
 describe('User Endpoint', () => {
   let userID;
+  let userToken;
+
+  // Move userData to the outer scope
+  const userData = {
+    email: 'npm@example.test',
+    password: '02356', 
+  };
 
   beforeEach(async () => {
-    await  dbClient.client.db().collection('users').deleteMany({});
+    await dbClient.client.db().collection('users').deleteMany({});
   });
 
   describe('POST /users', () => {
-    const userData = {
-      email: 'npm@example.test',
-      password: '02356', 
-    };
-
     it('should create a new user', async () => {
       const res = await request(app).post('/users').send(userData);
       expect(res.status).to.equal(201);
@@ -38,6 +39,32 @@ describe('User Endpoint', () => {
       const res = await request(app).post('/users').send(userData);
       expect(res.status).to.equal(400);
       expect(res.body).to.have.property('error', 'Already exist');
+    });
+  });
+
+  describe('GET /users/me', () => {
+    before(async () => {
+      const res = await request(app).get('/connect').auth(userData.email, userData.password);
+      userToken = res.body.token;
+    });
+
+    it('should return authenticated user information', async () => {
+      const res = await request(app).get('/users/me').set('X-Token', userToken);
+      expect(res.status).to.equal(200);
+      expect(res.body).to.have.property('id', userID);
+      expect(res.body).to.have.property('email', userData.email);
+    });
+
+    it('should return 401 for unauthorized (no token)', async () => {
+      const res = await request(app).get('/users/me').set('x-token', '');
+      expect(res.status).to.equal(401);
+      expect(res.body).to.have.property('error', 'Unauthorized');
+    });
+
+    it('should return 401 for unauthorized (no user id found)', async () => {
+      const res = await request(app).get('/users/me').set('x-token', 'notAToken');
+      expect(res.status).to.equal(401);
+      expect(res.body).to.have.property('error', 'Unauthorized');
     });
   });
 
