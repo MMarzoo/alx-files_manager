f16b6ce7aeb9e8c44865d02e51df73448707473d
MMarzoo <mmarzouk12378@gmail.com>
2024-10-02 17:53:00 +0300

    Task#10: POST /files

---
+++ tests/controllers/FilesController.test.js
@@ -0,0 +1,80 @@
+import request from 'supertest';
+import { expect } from 'chai';
+import app from '../../server';
+import dbClient from '../../utils/db';
+import redisClient from '../../utils/redis';
+import { ObjectId } from 'mongodb';
+
+describe('File Endpoint', () => {
+  let userId;
+  let token;
+  let fileId;
+
+  const userData = {
+    email: 'test@example.com',
+    password: 'password321',
+  };
+
+  const fileData = {
+    name: 'testexample.txt',
+    type: 'file',
+    data: Buffer.from('Hello, World!').toString('base64'),
+  };
+
+  before(async () => {
+    const resId = await request(app).post('/users').send(userData);
+    console.log('User creation response:', resId.body);
+    userId = resId.body.id;
+    console.log('User ID:', userId);
+
+    const resToken = await request(app).get('/connect').auth(userData.email, userData.password);
+    token = resToken.body.token;
+    console.log('Token:', token);
+  });
+
+  describe('POST /files', () => {
+    it('should upload a new file', async () => {
+      const res = await request(app)
+        .post('/files')
+        .set('X-Token', token)
+        .send(fileData);
+  
+        // console.log(res.body);
+  
+      expect(res.status).to.equal(201);
+      expect(res.body).to.have.property('id').that.is.a('string');
+      expect(res.body).to.have.property('userId', userId);
+      expect(res.body).to.have.property('name', fileData.name);
+      expect(res.body).to.have.property('type', fileData.type);
+      expect(res.body).to.have.property('isPublic', false);
+      expect(res.body).to.have.property('parentId', 0);
+      fileId = res.body.id;
+    });
+
+    it('should return 401 for unauthorized (no token)', async () => {
+      const res = await request(app).put(`/files/${fileId}/unpublish`);
+      expect(res.status).to.equal(401);
+      expect(res.body).to.have.property('error', 'Unauthorized');
+    });
+
+    it('should return 401 for unauthorized (no userId found)', async () => {
+      const res = await request(app).put(`/files/${fileId}/unpublish`).set('x-token', 'notAToken');
+      expect(res.status).to.equal(401);
+      expect(res.body).to.have.property('error', 'Unauthorized');
+    });
+
+    after(async () => {
+      await dbClient.client.db().collection('users').deleteOne({ email: userData.email });
+  
+      if (fileId) {
+        await dbClient.client.db().collection('files').deleteOne({ _id: ObjectId(fileId) });
+      }
+  
+      // if (folderId) {
+      //   await dbClient.client.db().collection('files').deleteOne({ _id: ObjectId(folderId) });
+      // }
+  
+      await redisClient.del(`auth_${token}`);
+    });
+  });
+});
